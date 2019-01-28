01eaada91aafc3a717ed9090972d2f64842c70ce
Jenna Mills <superangelfish555@gmail.com>
2019-01-28 08:48:19 -0700

    Initial commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Mills, Jenna on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
