119445101a14916dc383e0a790e33dadd0ed83c5
Jenna Mills <superangelfish555@gmail.com>
2019-01-30 09:34:28 -0700

    Created the Array class and created constructor method.

---
+++ DataStructures/Model/Linear/Array.hpp
@@ -0,0 +1,49 @@
+//
+//  Array.hpp
+//  DataStructures
+//
+//  Created by Mills, Jenna on 1/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <assert.h> //used to validate user supplied data
+#include <iostream> //used to trace and debug statements
+
+using namespace std;      //used for keyword access
+
+template <class Type>
+class Array
+{
+private:
+    int size;
+    Type * internalArray;
+public:
+    //constructor
+    Array<Type>(int size);
+    //copy constructor
+    Array<Type>(const Array<Type> & toCopy);
+    //destructor
+    ~Array<Type>();
+    //operators
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    //methods
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+};
+
+//constructor
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    internalArray = new Type[size];
+}
+
+#endif /* Array_hpp */
---
+++ DataStructures/Model/Nodes/LinearNode.hpp
@@ -0,0 +1,57 @@
+//
+//  LinearNode.hpp
+//  DataStructures
+//
+//  Created by Mills, Jenna on 1/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include <assert.h>
+
+template <classType>
+class linearNode : public Node<Type>
+{
+private:
+    LinearNode<Type> * next;
+public:
+    LinearNode(Type data);
+    LInearNode(Type data, LinearNode<Type> * next);
+    LinearNode<Type> * getNext();
+    void setNext(LinearNode<Type> * next);
+};
+
+//Implementation of templates
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node()
+{}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type> (data)
+{
+    this->next = next;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNext()
+{
+    return this->next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNext(LinearNode<Type> * next)
+{
+    this->next = next;
+}
+
+
+
+#endif /* LinearNode_hpp */
