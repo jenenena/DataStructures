e00a3c57d5e2565d474dd082250264abe376aa1f
Jenna Mills <superangelfish555@gmail.com>
2019-02-07 08:38:35 -0700

    Finished List.hpp

--- DataStructures/Model/Linear/List.hpp
+++ DataStructures/Model/Linear/List.hpp
@@ -9,5 +9,25 @@
 #ifndef List_hpp
 #define List_hpp
 
+#include "../Nodes/LinearNode.hpp"
+
+template <class Type>
+class List
+{
+protected:
+    int size;
+public:
+    //STRUCTURE; methods to actually deal with the list
+    virtual void add(Type item) = 0;
+    //Virtual means that the method can be overwritten; assigning the method to be zero means
+    // that the method is abstract. The method is empty.
+    virtual void addAtIndex(int index; Type item) = 0;
+    virtual Type remove(int index ) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //HELPER; methods to ask the list
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
 
 #endif /* List_h */
