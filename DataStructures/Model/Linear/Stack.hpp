4f35282843a8ebcf27ed13ef834bc84e655f5d6c
Jenna Mills <superangelfish555@gmail.com>
2019-02-13 08:50:45 -0700

    Added methods to Stack!

--- DataStructures/Model/Linear/Stack.hpp
+++ DataStructures/Model/Linear/Stack.hpp
@@ -14,6 +14,7 @@
 template <class Type>
 class Stack : public LinkedList<Type>
 {
+public:
     Stack();
     ~Stack();
     
@@ -33,6 +34,80 @@ template <class Type>
 Stack<type> :: Stack() : LinkedList<Type>()
 {
     //Empty
+}
+
+template <class Type>
+Stack<Type> :: ~Stack()
+{
+    while(this->size > 0)
+    {
+        pop();
+    }
+}
+
+template <class Type>
+void Stack<Type> :: push(Type data)
+{
+    LinearNode<Type> * add = new LinearNode<Type>(data);
+    
+    if(this->size = 0)
+    {
+        this->end = add;
+    }
+    else
+    {
+        add->setNextNode(this->front);
+    }
+    this->front = add;
+    this->size++;
+}
+
+template <class Type>
+void Stack<Type> :: add(Type data)
+{
+    push(data);
+}
+
+template <class Type>
+void Stack<Type> :: addAtIndex(int index, Type data)
+{
+    assert(index == 0);
+    push(data);
+}
+
+template <class Type>
+Type Stack<Type> :: pop()
+{
+    assert (this->size > 0);
+    type removed = this->front->getData();
+    
+    LinearNode<Type> * removedNode = this->getFront();
+    this->front = removedNode->getNextNode();
+    delete removedNode;
+    
+    this->size--;
+    return removed;
+}
+
+template <class Type>
+Type Stack<Type> :: remove(int index)
+{
+    assert(index == 0);
+    return pop();
+}
+
+template <class Type>
+Type Stack<Type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Stack<Type> :: getFromIndex(int index)
+{
+    assert(index == 0);
+    return peek();
 }
 
 #endif /* Stack_hpp */
